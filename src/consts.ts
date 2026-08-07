75ac61662657ffb4319df28125532ad9d6a063eb
安倉大地 <akuradaichi@MacBookAir.lan>
2026-08-07 13:50:43 +0900

    Update blog content and add Google Analytics

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "気になるニュースブログ";
+
+export const SITE_DESCRIPTION =
+"テレビやネットで話題になったニュースを、「なぜ？」「どうして？」という視点から分かりやすく解説するブログです。";
